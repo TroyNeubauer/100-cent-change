7f90650d1ed4329741aad72a9ef111e0b00fef43
Troy Neubauer <troyneubauer@gmail.com>
2021-01-27 10:34:29 -0500

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate polynomial;
+
+use polynomial::Polynomial;
+use Vec;
+
+
+fn main() {
+    println!("Hello, world!");
+    
+    let coins = vec![1, 5, 10, 25, 50, 100];
+
+    let target = 100; // How many ways can we make 100 cents
+
+    let coinPolys: Vec<Polynomial> = Vec::new();
+
+}
